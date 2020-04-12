82014d35111f86e1da455ffca4564a1486e82a7a
htkien <htkien2511@gmail.com>
2020-04-12 20:40:01 +0700

    Initial Commit

---
+++ FirebaseiOSDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseiOSDemo
+//
+//  Created by Hoang Trong Kien on 4/12/20.
+//  Copyright © 2020 Hoang Trong Kien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
